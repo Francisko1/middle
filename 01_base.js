d9f1b2fe842b3b3ba81860ea33e961d903a1b8a2
Francisko1 <140095139+Francisko1@users.noreply.github.com>
2023-07-20 18:42:58 +0300

    Add files via upload

---
+++ 01_base.js
@@ -0,0 +1,77 @@
+
+//const isReady = true
+//if (isReady){
+//    console.log('Скрипт работает')
+//} else {
+//console.log('Ничего не готово')}
+
+// Тернарное выражение
+ //isReady ? console.log('Скрипт работает') : console.log('Ничего не готово')
+
+ // функции
+
+// function calculatedAge(year) {
+    // return 2023 - year
+// }
+// console.log(calculatedAge(1995))
+// console.log(calculatedAge(1983))
+//console.log(calculatedAge(1954))
+//function logInfoAbout(name, year){
+    //const age = calculatedAge(year)
+    //if (age > 0)
+    //console.log('человек по имени ' + name + ' сейчас имеет возвраст: ' + age)
+    //else {
+        //console.log('Вообще-то это уже будущее!')
+    //}
+//}
+//logInfoAbout('Roman', 1976)
+//logInfoAbout('Ivan', 1995)
+//logInfoAbout('Ivan', 1995)
+//
+// 9. Массивы
+//const cars = ['мерс', 'бмв','опель']
+//const cars = new Array('мерс', 'бмв','опель')
+//console.log(cars.length)
+
+//console.log(cars[1])
+//console.log(cars[0])
+//console.log(cars[2])
+//cars[0] = 'Porshe'
+//console.log(cars)
+//cars[3] = 'Mazda' 
+// cars[cars.length] = 'Mazda'
+//console.log(cars)
+
+// 10. ЦИКЛЫ
+//const cars = ['мерс', 'бмв', 'опель', 'Mazda', 'Porshe']
+
+//for(let i = 0; i < cars.length; i++){
+//   const car = cars[i]
+//   console.log(car)
+//}
+
+//for(let car of cars){
+//    console.log(car)
+//}
+
+// 11. ОБЪЕКТЫ
+// const person = {
+//     firstName: 'Roman',
+//     lastName: 'Reshetnik',
+//     year: '1976',
+//     languages: ['ru', 'en', 'es', 'ucr'],
+//     hasWife: false,
+//     greet: function(){
+//         console.log('greet from person')
+//     }
+// }
+// //console.log(person)
+// console.log(person.firstName)
+// console.log(['lastName'])
+// const key = 'year'
+// console.log(person [key])
+// person.hasWife = true
+// person.isProgrammer = true
+// console.log(person)
+
+// person.greet()
